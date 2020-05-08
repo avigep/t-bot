31fa81e792297937e36d27476ddc6ae8d591bfea
avigep <avi.gep@gmail.com>
2020-05-08 13:54:08 +0530

    refactor text_interpreter_service

--- app/services/text_interpreter_service.rb
+++ app/services/text_interpreter_service.rb
@@ -9,90 +9,33 @@ class TextInterpreterService
   def execute
     @result = case @intent
               when :incoming_transaction, :outgoing_transaction
-                transaction = {
-                  target_name: @wit_response['entities']['contact'].first['value'].downcase,
-                  amount: @wit_response['entities']['number'].first['value'],
-                  notes: @wit_response['_text'],
-                  intent: @intent,
-                  raw_params: @params
-                }
-                TransactionAddJob.new(transaction).perform
+                TransactionAddJob.new(transaction_params).perform
               when :report_daily
-                # TODO add contact as param
-                DailyReportJob.new({to: @params['From'], from: @params['To'], contact: ''}).perform
+                DailyReportJob.new(daily_report_params).perform
               else
-                # TODO move to twilio service
-                TwilioService.deliver(
-                  {
-                     type: :undefined,
-                     result: :fail,
-                     message: EmptyTarget.message,
-                     from: @params['To'],
-                     to: @params['From']
-                  }
-                )
-                :fail
+                TwilioService.deliver_menu(from: @params['To'], to: @params['From'])
+                :fail_served_menu
               end
-    { "message": @result.to_s }
+    { "response": @result.to_s }
   end
 
   private
 
-  def transaction_handler
-    transaction = Transaction.new(transaction_attributes)
-    result = transaction.save! ? :success : :fail
-    if result == :success
-      TransactionNotificationJob.new(transaction).perform
-    else
-      Rails.logger.error("Error saving transaction:  #{transaction.errors}")
-    end
-    result
-    # transaction = Transaction.new(transaction_attributes)
-    # result = transaction.save! ? :success : :fail
-    # TwilioService.deliver(
-    #   {
-    #     type: :transaction,
-    #     result: result,
-    #     target: transaction,
-    #     from: @params['To'], # Yes I know!
-    #     to: "whatsapp:#{transaction.from.contact_numbers.first}"
-    #   }
-    # )
-
-    # TwilioService.deliver(
-    #   {
-    #     type: :transaction,
-    #     result: result,
-    #     target: transaction,
-    #     from: @params['To'],
-    #     to: "whatsapp:#{transaction.to.contact_numbers.first}"
-    #   }
-    # )
-    # result
+  def daily_report_params
+    { to: @params['From'], from: @params['To'] }
   end
 
-  def transaction_attributes
-    case @intent.to_sym
-    when :incoming_transaction
-     to = Member.find_by(contact_numbers: @params['From'].split(':').last)
-     from = Member.find_or_create_by(name: @wit_response['entities']['contact'].first['value'].downcase)
-    when :outgoing_transaction
-     to = Member.find_or_create_by(name: @wit_response['entities']['contact'].first['value'].downcase)
-     from = Member.find_by(contact_numbers: @params['From'].split(':').last)
-    else
-      to = nil
-      form = nil
-    end
+  def transaction_params
     {
-      to: to,
-      from: from,
+      target_name: @wit_response['entities']['contact'].first['value'].downcase,
       amount: @wit_response['entities']['number'].first['value'],
-      notes: @wit_response['_text']
+      notes: @wit_response['_text'],
+      intent: @intent,
+      raw_params: @params
     }
   end
 
   def wit_client
     @wit ||= Wit.new(access_token: ENV['wbot_wit_access_token'])
   end
-
 end
--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -9,6 +9,10 @@ class TwilioService
     )
   end
 
+  def self.deliver_menu(params)
+    deliver(message: EmptyTarget.message, from: params[:from], to: params[:to])
+  end
+
   def self.twilio_client
     @twilio_client ||= Twilio::REST::Client.new(ENV['wbot_twilio_account_sid'], ENV['wbot_twilio_auth_token'])
   end
